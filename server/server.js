e2ad49d7585eba8eaf12877a93285de8b28c1bba
bmp713 <bmp713@yahoo.com>
2022-07-10 16:11:38 -0700

    Node news API

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express(); 
+const axios = require("axios");
 const port = 5000; 
+const app = express(); 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,6 +22,33 @@ app.get("/", (req, res) => {
 
 // GET request
 app.get("/users", (req, res) => {
+    const users = [
+        { name: "user 1", email: "steve1@apple.com"},
+        { name: "user 2", email: "steve2@apple.com"},
+        { name: "user 3", email: "steve3@apple.com"}
+    ];
+    res.send(users);
+});
+
+
+// GET NY Times top headlines
+app.get("/news", async (req, res) => {
+    let data;
+    try{
+        await axios.get(`https://api.nytimes.com/svc/news/v3/content/nyt/all.json?api-key=${process.env.NY_TIMES_API_KEY}`)
+            .then((res) => {
+                //console.log("res =>", res.data.results[0]);
+                data = res.data;
+                console.log("data =>", data);
+            })
+            .catch( err => {
+                console.log(err);
+            });
+    }catch(err){
+        console.log(err);
+    }
+    res.send(data);
+
 });
 
 
